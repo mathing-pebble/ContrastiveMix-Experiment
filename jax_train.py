ed2934205085a2efb5d84b4985ddafa064104f14
mathing-pebble <118791131+mathing-pebble@users.noreply.github.com>
2024-06-25 03:18:44 +0900

    Update jax_train.py

--- jax_train.py
+++ jax_train.py
@@ -35,9 +35,6 @@ def main():
         model_args, data_args, training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
     else:
         model_args, data_args, training_args = parser.parse_args_into_dataclasses()
-        model_args: ModelArguments
-        data_args: DataArguments
-        training_args: TevatronTrainingArguments
 
     if (
             os.path.exists(training_args.output_dir)
